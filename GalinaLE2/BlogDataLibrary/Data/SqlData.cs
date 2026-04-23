7b68699ca78ccbefbbd4364171bc14d163a4e268
John Galina <johngalina23@gmail.com>
2026-04-23 23:38:51 +0800

    Upload project files

---
+++ GalinaLE2/BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,18 @@
+﻿using BlogDataLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogDataLibrary.Data
+{
+    internal class SqlData
+    {
+        private ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
